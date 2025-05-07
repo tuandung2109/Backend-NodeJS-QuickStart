93a27af8c1fe2a2cbcdf09e67a2ee95d838e1025
tuandung2109 <dinhtuandung10a6@gmail.com>
2025-05-07 07:57:29 +0700

    Initial commit - reinitialized project

---
+++ src/controllers/homeController.js
@@ -0,0 +1,30 @@
+import e from 'express';
+import db from '../models/index';
+let getHomePage = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
+
+        return res.render('homepage.ejs' , {
+            data : JSON.stringify(data) 
+        });
+    } catch (error) {
+        console.log(e);
+        
+    }
+
+
+    return res.render('homepage.ejs');
+}
+
+let getAboutPage = (req, res) => {
+    return res.render('test/about.ejs');
+}
+
+// object: {
+//     key: '',
+//     value: ''
+// }
+module.exports = {
+    getHomePage: getHomePage,
+    getAboutPage: getAboutPage
+}
