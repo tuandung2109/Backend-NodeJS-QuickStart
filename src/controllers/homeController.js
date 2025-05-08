7df97b4469167d9bbdb0c25bdd6013e56cec34ee
tuandung2109 <dinhtuandung10a6@gmail.com>
2025-05-09 01:01:37 +0700

    #25 Update homeController, web routes, add services and CRUD view

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,7 @@
 import e from 'express';
 import db from '../models/index';
+import CRUDService  from '../services/CRUDService';
+
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
@@ -20,11 +22,20 @@ let getAboutPage = (req, res) => {
     return res.render('test/about.ejs');
 }
 
-// object: {
-//     key: '',
-//     value: ''
-// }
+let getCRUD = (req, res) => { 
+    return res.render('crud.ejs');
+}
+
+let postCRUD = async (req , res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send('post crud from server');
+}
+
+
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage: getAboutPage
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
 }
