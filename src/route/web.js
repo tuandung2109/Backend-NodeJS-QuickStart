c82192a746bfaa7712f948d699081ddf7c125646
tuandung2109 <dinhtuandung10a6@gmail.com>
2025-05-09 14:59:20 +0700

    #27 Sử Dụng ORM Để Sửa Dữ liệu (Edit)

--- src/route/web.js
+++ src/route/web.js
@@ -7,9 +7,12 @@ let initWebRoutes = (app) => {
     router.get('/', homeController.getHomePage);
     router.get('/about', homeController.getAboutPage);
     router.get('/crud', homeController.getCRUD);
+    
     router.post('/post-crud', homeController.postCRUD);
     router.get('/get-crud', homeController.displayGetCRUD);
-    
+    router.get('/edit-crud', homeController.getEditCRUD);
+    router.post('/put-crud', homeController.putCRUD);
+
     return app.use("/", router);
 }
 
