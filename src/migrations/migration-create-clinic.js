59954c2e36f1dd577f210388d1fb5931a87d0c1b
tuandung2109 <dinhtuandung10a6@gmail.com>
2025-05-25 20:33:08 +0700

    Add comment to test graphline

--- src/migrations/migration-create-clinic.js
+++ src/migrations/migration-create-clinic.js
@@ -1,3 +1,45 @@
+// 'use strict';
+
+// const { name } = require("ejs");
+
+// module.exports = {
+//   up: async (queryInterface, Sequelize) => {
+
+//     await queryInterface.createTable('clinics', {
+//       id: {
+//         allowNull: false,
+//         autoIncrement: true,
+//         primaryKey: true,
+//         type: Sequelize.INTEGER
+//       },
+//       address: {
+//         type: Sequelize.STRING
+//       },
+//       name: {
+//         type: Sequelize.STRING
+//       },
+//       decreption: {
+//         type: Sequelize.TEXT
+//       },
+//       image: {
+//         type: Sequelize.STRING
+//       },
+//       createdAt: {
+//         allowNull: false,
+//         type: Sequelize.DATE
+//       },
+//       updatedAt: {
+//         allowNull: false,
+//         type: Sequelize.DATE
+//       }
+//     });
+//   },
+//   down: async (queryInterface, Sequelize) => {
+//     await queryInterface.dropTable('clinics');
+//   }
+// };
+
+
 'use strict';
 
 const { name } = require("ejs");
