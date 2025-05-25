015de879c66d29d8b0df366e80418dd6febdeba8
tuandung2109 <dinhtuandung10a6@gmail.com>
2025-05-25 20:41:13 +0700

    Delete commnent

--- src/models/schedule.js
+++ src/models/schedule.js
@@ -1,31 +1,3 @@
-// 'use strict';
-// const {
-//   Model
-// } = require('sequelize');
-// module.exports = (sequelize, DataTypes) => {
-//   class Schecule extends Model {
-//     /**
-//      * Helper method for defining associations.
-//      * This method is not a part of Sequelize lifecycle.
-//      * The `models/index` file will call this method automatically.
-//      */
-//     static associate(models) {
-//       // define association here
-//     }
-//   };
-//   Schecule.init({
-//     currentNumber: DataTypes.INTEGER,
-//     maxNumber: DataTypes.INTEGER,
-//     date: DataTypes.DATE,
-//     timeType: DataTypes.STRING,
-//     doctorId: DataTypes.INTEGER,
-//   }, {
-//     sequelize,
-//     modelName: 'Schecule',
-//   });
-//   return Schecule;
-// };
-
 'use strict';
 const {
   Model
